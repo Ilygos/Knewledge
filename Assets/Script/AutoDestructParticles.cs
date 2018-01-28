5bca300f833a7869473a31cd16058e38294692cb
MaxenceJarvis <maxence.beaumont6@gmail.com>
2018-01-28 12:24:23 +0100

    푸른 팀 승리

---
+++ Assets/Script/AutoDestructParticles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestructParticles : MonoBehaviour {
+
+    ParticleSystem _particles;
+
+    // Use this for initialization
+    void Start()
+    {
+        _particles = GetComponent<ParticleSystem>();
+        _particles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_particles.isPlaying) DestroyObject(gameObject);
+    }
+}
---
+++ Assets/Script/Loading.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+
+    public Image tutoPanno;
+
+    public Sprite tuto1;
+    public Sprite tuto4;
+    public Sprite tuto3;
+    public Sprite tuto2;
+
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(startGame());
+	}
+
+    IEnumerator tuto1display()
+    {
+        tutoPanno.sprite = tuto1;
+        yield return new WaitForSeconds(0.66f);
+        StartCoroutine(tuto2display());
+    }
+
+    IEnumerator tuto2display()
+    {
+        tutoPanno.sprite = tuto1;
+        yield return new WaitForSeconds(0.66f);
+        StartCoroutine(tuto3display());
+    }
+
+    IEnumerator tuto3display()
+    {
+        tutoPanno.sprite = tuto1;
+        yield return new WaitForSeconds(0.66f);
+        StartCoroutine(tuto4display());
+    }
+
+    IEnumerator tuto4display()
+    {
+        tutoPanno.sprite = tuto1;
+        yield return new WaitForSeconds(0.66f);
+    }
+
+    IEnumerator startGame()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("Test LD");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
